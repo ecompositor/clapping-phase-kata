398ada28bc0bc92c89783b58a2ba4f7e88c1ded8
Richard Ashworth <rich@Richards-MacBook-Pro.local>
2016-01-03 23:15:06 +0000

    refactored main App into ClappingMusic object

--- src/main/scala/com/richashworth/clappingmusic/ClappingMusic.scala
+++ src/main/scala/com/richashworth/clappingmusic/ClappingMusic.scala
@@ -1,7 +1,7 @@
 package com.richashworth.clappingmusic
 
 object ClappingMusic extends App {
-  val musician = new MidiMusician(9)
+  val midiPlayer = new MidiPlayer(9)
   val originalPattern: Pattern = new Pattern("XXX XX X XX ")
-  musician.play(Composer.composeTwoPartPhaseMusic(originalPattern))
+  midiPlayer.play(Composer.composeTwoPartPhaseMusic(originalPattern))
 }
