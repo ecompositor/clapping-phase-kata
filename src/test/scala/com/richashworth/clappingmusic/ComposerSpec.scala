dba50262a9e711bd31bbae8d54df50f93cbefbbb
Richard Ashworth <rich@Richards-MacBook-Pro.local>
2016-03-28 19:19:30 +0100

    change build system to gradle

---
+++ src/test/scala/com/richashworth/clappingmusic/ComposerSpec.scala
@@ -0,0 +1,14 @@
+package com.richashworth.clappingmusic
+
+import org.scalatest.FlatSpec
+
+/**
+  * Created by rich on 23/12/2015.
+  */
+class ComposerSpec extends FlatSpec {
+
+  "The Composer object" should "correctly shift a pattern by one beat" in {
+    assert(true)
+  }
+
+}
