054bdebb1de0f46c446894252c1a9e94b29e3e25
Richard Ashworth <rich@Richards-MacBook-Pro.local>
2016-03-30 22:49:28 +0100

    alignment

--- src/main/scala/com/richashworth/clappingmusic/Musician.scala
+++ src/main/scala/com/richashworth/clappingmusic/Musician.scala
@@ -33,25 +33,27 @@ case object Ping
 object Main extends App {
   val synthesizer = MidiSystem.getSynthesizer()
   synthesizer.open()
+
   val channel = synthesizer.getChannels()(9)
 
-  val system = ActorSystem("MusicianSystem")
+  val system       = ActorSystem("MusicianSystem")
   val midiActorOne = system.actorOf(Props(new Musician(channel, 43)), name = "A")
   val midiActorTwo = system.actorOf(Props(new Musician(channel, 35)), name = "B")
-  val maxPlayingTime = 1 hour
+
+  val maxPlayingTime   = 1 hour
   implicit val timeout = Timeout(maxPlayingTime)
 
-  val clappingMusic = Composer.composeTwoPartPhaseMusic(new Phrase("XXX XX X XX "))
+  val clappingMusic    = Composer.composeTwoPartPhaseMusic(new Phrase("XXX XX X XX "))
   val phaseRepetitions = 8
 
-  clappingMusic.foreach(duet ⇒ {
-    (1 until phaseRepetitions).foreach(_ ⇒ {
+  clappingMusic.foreach(duet ⇒
+    (1 until phaseRepetitions).foreach(_ ⇒
       (0 until duet._1.length).foreach(i ⇒ {
         midiActorOne ! duet._1.beats(i)
         midiActorTwo ! duet._2.beats(i)
       })
-    })
-  })
+    )
+  )
 
   val futures = Future sequence Seq(midiActorOne ? Ping, midiActorTwo ? Ping)
   Await.ready(futures, maxPlayingTime)
